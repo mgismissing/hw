6bde79ee65417cbce38de61f0e865d9fb5622a47
mgismissing <gabrycraft2010@gmail.com>
2024-04-15 22:59:13 +0200

    Added debug

--- login/after/login.js
+++ login/after/login.js
@@ -13,6 +13,8 @@ const id = /id=([^&#=]*)/.exec(window.location.search)[1];
 fetch("https://auth.empties.it/login/2893/id.txt")
   .then(response => response.text())
   .then(text => {
+    console.log(id);
+    console.log(text);
     if (id == text) {
         console.log("id check passed")
         if (users.indexOf(u) > -1 && passwords[users.indexOf(u)] == p) {
